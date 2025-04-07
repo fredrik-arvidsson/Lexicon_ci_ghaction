27b8313597e4b8dff4d6f84406b17792f5b90461
fredrikthewhite <82776341+fredrikthewhite@users.noreply.github.com>
2025-04-08 00:52:41 +0200

    added automated tests

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,20 @@
+import com.FutureSoft.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    void testAdd() {
+
+        Calculator calc = new Calculator();
+
+        int result = calc.add(3, 5);
+
+        assertEquals( 8, result);
+
+
+    }
+
+}
